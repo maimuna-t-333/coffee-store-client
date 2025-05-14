6cb202291c0e8496ea39729b528d93f1fc799bee
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-05-14 21:14:16 +0600

    Coffee store client demand document storing

---
+++ src/Components/AddCoffee.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+
+const AddCoffee = () => {
+    const handleAddCoffee=(e)=>{
+        e.preventDefault();
+        const form=e.target
+        const formData=new FormData(form)
+        const newCoffee=Object.fromEntries(formData.entries())
+        console.log(newCoffee)
+
+        //send coffee data to the db
+
+        fetch('http://localhost:3000/coffees',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(newCoffee)
+
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log('after adding coffee to db',data)
+        })
+    }
+    return (
+        <div className='p-24'>
+            <div className='p-12 space-y-4 text-center '>
+                <h1 className='text-6xl'>Add Coffee</h1>
+                <p>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
+            </div>
+            <form onSubmit={handleAddCoffee} >
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+
+                
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+                    
+
+                    <label className="label">Name</label>
+                    <input type="text" className="input w-full" placeholder="Enter Coffee Name" name='name' />
+
+                </fieldset>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+                    
+
+                    <label className="label">Quantity</label>
+                    <input type="text" className="input w-full" placeholder="Enter quantity" name='quantity' />
+
+                </fieldset>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+                    
+
+                    <label className="label">Suppiler</label>
+                    <input type="text" className="input w-full" placeholder="Enter Coffee supplier" name='supplier' />
+
+                </fieldset>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+                    
+
+                    <label className="label">Taste</label>
+                    <input type="text" className="input w-full" placeholder="Enter Coffee taste" name='tase' />
+
+                </fieldset>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+                    
+
+                    <label className="label">Catagory</label>
+                    <input type="text" className="input w-full" placeholder="Enter Coffee catagory" name='catagory' />
+
+                </fieldset>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+                    
+
+                    <label className="label">Details</label>
+                    <input type="text" className="input w-full" placeholder="Enter Coffee details" name='details' />
+
+                </fieldset>
+                
+              
+                </div>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4 my-6">
+                    
+
+                    <label className="label">Photo URL</label>
+                    <input type="text" className="input w-full" placeholder="Enter photo url" name='photo' />
+
+                </fieldset>
+                <input type="submit" className='btn w-full' value="Add coffee" />
+            </form>
+
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Header from '../Components/Header';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Header></Header>
+            <div className='w-7xl mx-auto'>
+                <Outlet></Outlet>
+            </div>
+            
+        </div>
+    );
+};
+
+export default MainLayout;
